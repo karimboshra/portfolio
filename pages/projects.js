a8de371382bd5125bd205f798a8b9a55c3301a9e
S4DD4M <nazo_magarian@yahoo.com>
2023-05-09 20:19:10 -0400

    examples of usage

--- pages/projects.js
+++ pages/projects.js
@@ -2,8 +2,11 @@ import React, { Fragment } from 'react';
 import getConfig from 'next/config'
 import Link from 'next/link';
 import { Footer } from '../components/Footer';
-import { links, SEO, } from '../config/config';
+import { links, SEO, navigation } from '../config/config';
 import { Header } from '../components/Header';
+import image from './lucp2.png';
+import { Nav } from '../components/Navbar';
+//import video from './chunky.mp4';
 
 const { publicRuntimeConfig } = getConfig()
 
@@ -11,16 +14,24 @@ export default function Home() {
   return (
     <Fragment>
       <Header seo={SEO} />
+      <Nav
+        title={navigation.name}
+        links={navigation.links}
+      />
       <div className="d-flex flex-column justify-content-between bg-secondary min-vh-100">
         <div className="py-5 px-5 container text-center">
-          <img className="img-fluid my-3 card-image" width="150" height="150" src={links.image} alt="profile of hashirshoaeb" />
+          <img className="" width="150" height="150" src={links.image} alt="profile of hashirshoaeb" />
           <h3 className="mt-3">{links.title}</h3>
-          <p>{links.description}</p>
-
+          <p className="flex-start">{links.description}</p>
+          <p className='textleft bottomBorder'>This is a test2</p>
+          <img src={image.src} alt="Jean-Francois smoking a fat cogie" />
+          <video width="750" height="500" controls >
+            <source src="/Videos/chunky.mp4" type="video/mp4" />
+          </video>
         </div>
-		<div className="container">
-			<p>Peni</p>
-		</div>
+        <div className="">
+          <p>Peni</p>
+        </div>
         <Footer />
       </div>
     </Fragment>
